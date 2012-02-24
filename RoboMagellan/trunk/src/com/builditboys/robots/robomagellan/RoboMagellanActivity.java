bee4b802d0244d61b081ba138fea14aadaf3d96d
wabrew <wabrew@df9d05b4-6b32-7a60-05c0-da90c346198a>
2012-02-24 23:39:31 +0000

    Initial Checkin

---
+++ RoboMagellan/trunk/src/com/builditboys/robots/robomagellan/RoboMagellanActivity.java
@@ -0,0 +1,13 @@
+package com.builditboys.robots.robomagellan;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RoboMagellanActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
