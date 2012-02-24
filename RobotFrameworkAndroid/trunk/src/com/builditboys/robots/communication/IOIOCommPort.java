e5b6c9245052ededfc64916d004bd2a15fcc39d0
wabrew <wabrew@df9d05b4-6b32-7a60-05c0-da90c346198a>
2012-02-24 23:45:14 +0000

    Initial checkin

---
+++ RobotFrameworkAndroid/trunk/src/com/builditboys/robots/communication/IOIOCommPort.java
@@ -0,0 +1,28 @@
+package com.builditboys.robots.communication;
+
+public class IOIOCommPort implements CommPortInterface {
+	
+	public byte readByte () {
+		return 0;
+	}
+	
+//	public byte[] readBytes () {
+//		return new byte[0];
+//	}
+	
+	public void writeByte (byte bite) {
+		
+	}
+	
+	public void writeBytes (byte bites[]) {
+		
+	}
+	
+	public void tossInput () {
+	}
+	
+	public void tossOutput () {
+	}
+
+
+}
