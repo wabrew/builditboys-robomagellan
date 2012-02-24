3abbe413172c7dfe9e946c0820406a78845dbf83
wabrew <wabrew@df9d05b4-6b32-7a60-05c0-da90c346198a>
2012-02-24 23:50:23 +0000

    Initial checkin

---
+++ RobotTestAndroid/trunk/src/com/builditboys/robots/RobotTestAndroidActivity.java
@@ -0,0 +1,13 @@
+package com.builditboys.robots;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RobotTestAndroidActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
